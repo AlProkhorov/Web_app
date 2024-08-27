4ec03f30a10b7ba853a3d944f58b04577556a7a8
alpro <alprokh64@gmail.com>
2024-08-27 14:13:57 +0300

    2st step

--- main.py
+++ main.py
@@ -20,12 +20,15 @@ def start_my_server():
 
 def load_page_from_det_request(request_data):
     HDRS = 'HTTP/1.1 200 OK\r\nContent -Type: text/html; charset=utf=8\r\n\r\n'
+    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent -Type: text/html; charset=utf=8\r\n\r\n'
     path = request_data.split(" ")[1]
     response = ''
-    with open('./' + path, 'rb') as file:
-        response = file.read()
-    return HDRS.encode('utf -8') + response
-
+    try:
+        with open('./' + path, 'rb') as file:
+            response = file.read()
+        return HDRS.encode('utf -8') + response
+    except FileNotFoundError:
+        return (HDRS_404 + 'Sorry, bro! No Page .....').encode('utf-8')
 
 if __name__ == '__main__':
     start_my_server()
