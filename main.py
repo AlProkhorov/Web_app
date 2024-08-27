3d81dcc5b9c63c7ae4636ea3e5a5d95e20fcfa2b
alpro <alprokh64@gmail.com>
2024-08-27 14:07:06 +0300

    2st step

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 2000))
-server.listen(4)
-print('Working....')
-client_socket, address = server.accept()
-data = client_socket.recv(1024).decode('utf -8')
-print(data)
-HDRS = 'HTTP/1.1 200 OK\r\nContent -Type: text/html; charset=utf=8\r\n\r\n'
-content = 'Well done, man....'.encode('utf -8')
-client_socket.send(HDRS.encode('utf-8') + content)
-print('shutdown this server')
+
+def start_my_server():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 2000))
+        server.listen(4)
+        while True:
+            print('Working....')
+            client_socket, address = server.accept()
+            data = client_socket.recv(1024).decode('utf -8')
+            content = load_page_from_det_request(data)
+            client_socket.send(content)
+            client_socket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+        print('shutdown this server')
+
+
+def load_page_from_det_request(request_data):
+    HDRS = 'HTTP/1.1 200 OK\r\nContent -Type: text/html; charset=utf=8\r\n\r\n'
+    path = request_data.split(" ")[1]
+    response = ''
+    with open('./' + path, 'rb') as file:
+        response = file.read()
+    return HDRS.encode('utf -8') + response
+
+
+if __name__ == '__main__':
+    start_my_server()
